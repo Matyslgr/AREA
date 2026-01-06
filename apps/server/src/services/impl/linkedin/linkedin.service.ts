0c56e7f3ec781929c979f5441df72eb0ed03c998
Matys Laguerre <matys.laguerre@epitech.eu>
2026-01-06 15:52:15 +0100

    feat(linkedin): add New Post action and Share Link reaction

--- apps/server/src/services/impl/linkedin/linkedin.service.ts
+++ apps/server/src/services/impl/linkedin/linkedin.service.ts
@@ -1,5 +1,7 @@
 import { IService } from '../../../interfaces/service.interface';
+import { LinkedInNewPostAction } from './actions/linkedin_new_post.action';
 import { LinkedInPostReaction } from './reactions/linkedin_post.reaction';
+import { LinkedInShareLinkReaction } from './reactions/linkedin_share_link.reaction';
 
 export const LinkedInService: IService = {
   id: 'linkedin',
@@ -7,8 +9,11 @@ export const LinkedInService: IService = {
   version: '1.0.0',
   description: 'Integration with LinkedIn social network.',
   is_oauth: true,
-  actions: [],
+  actions: [
+    LinkedInNewPostAction
+  ],
   reactions: [
-    LinkedInPostReaction
+    LinkedInPostReaction,
+    LinkedInShareLinkReaction
   ],
 };
